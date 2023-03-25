70511995788de49c87dd8060cb41855ec398036a
Ayush8923 <ayush.uniyal@coloredcow.in>
2023-03-25 22:39:40 +0530

    :art: save the data from the local storage

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,9 +8,11 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Requests\AuthRequest;
+use Illuminate\Support\Facades\Storage;
 
 class AuthController extends Controller
 {
+
     public function login(AuthRequest $request)
     {
         $user = User::where('email', $request->email)->first();
@@ -21,8 +23,26 @@ class AuthController extends Controller
             ]);
         }
 
-        return $user->createToken($request->device_name)->plainTextToken;
+        $token = $user->createToken($request->device_name)->plainTextToken;
+
+        // Store user information and token in local storage
+        $userData = [
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'token' => $token
+        ];
+
+        // Encode user data as JSON
+        $userDataJson = json_encode($userData);
+
+        // Save user data to local storage
+        Storage::put('user.json', $userDataJson);
+
+        return response()->json($userData);
+
     }
+
 
     public function register(AuthRequest $request)
     {
